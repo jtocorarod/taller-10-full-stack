575a900aebb51a31db2c9a2275ec8bfdfc8a9a00
jtocorarod <johann.tocora@gmail.com>
2023-10-25 08:25:12 -0500

    agregación index.html

--- punto-1/view/components/result.php
+++ punto-1/view/components/result.php
@@ -1,11 +1,11 @@
-<div class="result">
-<?php if (!empty($messageError)): ?>
-        <p class="error_message"><?php echo $messageError; ?></p>
-    <?php endif; ?>
-    
-    <?php if ($result != ""): ?>
-        <p><?php echo "Su resultado es: " .$result; ?></p>
-    <?php endif; ?>
+    <div class="result">
+    <?php if (!empty($messageError)): ?>
+            <p class="error_message"><?php echo $messageError; ?></p>
+        <?php endif; ?>
+        
+        <?php if ($result != ""): ?>
+            <p><?php echo "Su resultado es: " .$result; ?></p>
+        <?php endif; ?>
 
-  
-</div>
+    
+    </div>
